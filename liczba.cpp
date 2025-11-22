119193a343b3c92759b4222eec8175c928295f4e
Anton Jedrzejewski <207309941+maant1s@users.noreply.github.com>
2025-11-22 16:38:11 +0100

    added czy_doskonala

--- liczba.cpp
+++ liczba.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int main(){
+bool czy_doskonala(int x) {
+	int sum = 1;
+
+	for (int i = 2; i <= x / 2; i++)
+		if (x % i == 0) sum += i;
+
+	return sum == x;
+}
+
+int main() {
 	int x;
 	std::cin >> x;
-	std::cout << x << '\n';
+	std::cout << x << ((czy_doskonala(x)) ? " " : " nie ") << "jest liczba doskonala." << '\n';
 }
